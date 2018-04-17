3cfedc03aa414780f72cf34a43d76ec5ce748971
Robin van Poppel <Robin.van.Poppel@cz.nl>
2018-04-17 09:34:54 +0200

    First version of chocolately script for often used tools

---
+++ chocolately.ps1
@@ -0,0 +1,29 @@
+@echo off
+choco feature enable -n allowGlobalConfirmation
+
+choco install git.install
+choco install googlechrome
+choco install unchecky 
+choco install firefox
+choco install docker
+choco install 7zip.install
+
+choco install procexp
+choco install treesizefree
+choco install gitkraken
+choco install everything
+choco install sql-server-management-studio 
+choco install nimbletext 
+choco install sumatrapdf 
+choco install paint.net 
+choco install dotpeek 
+choco install inkscape 
+choco install javaruntime
+choco install soapui 
+choco install resharper
+choco install firacode
+
+REM manual choco install visualstudio
+REM manual choco install notepadplusplus.install
+REM manual	visual studio 	also setup fira code in visual studio
+REM manual	resharper		also install key
