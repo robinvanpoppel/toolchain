a597882e1cb3884389cc8eba4b50eeb8f21812f1
Robin van Poppel <github@nyaaa.nl>
2019-10-03 08:51:49 +0200

    Add export for currently installed visual studio code extensions

--- install-tools.ps1
+++ install-tools.ps1
@@ -30,3 +30,7 @@ REM manual setup WIN + N as New Window in Everything
 REM manual
 REM Visit nuget.org/downloads and select NuGet 3.3 or higher (2.8.6 is not compatible with Mono). The latest version is always recommended, and 4.1.0+ is required to publish packages to nuget.org.
 REM Download nuget.exe, create a tools folder, and add it to the PATH environment variable to use the CLI tool from anywhere.
+
+REM Visual Studio code extensions:
+REM Export:
+REM code --list-extensions | % { "code --install-extension $_" }
